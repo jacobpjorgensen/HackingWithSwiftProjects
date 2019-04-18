dfc57ba7e99753a44639e4870da0c55319b1f36c
Jacob Jorgensen <jacobpjorgensen@gmail.com>
2019-04-17 20:39:01 -0600

    Project 1

---
+++ Project 01 - Storm Viewer/StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailsViewController.swift
+//  StormViewer
+//
+//  Created by Jacob Jorgensen on 4/17/19.
+//  Copyright © 2019 Jacob Jorgensen. All rights reserved.
+//
+
+import UIKit
+
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+}
