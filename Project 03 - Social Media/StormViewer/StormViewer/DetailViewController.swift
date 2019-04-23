eb6919190780926c844053111c03a5330ba9bbd6
Jacob Jorgensen <jacobpjorgensen@gmail.com>
2019-04-22 19:53:10 -0600

    Project 3

---
+++ Project 03 - Social Media/StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailsViewController.swift
+//  StormViewer
+//
+//  Created by Jacob Jorgensen on 4/17/19.
+//  Copyright © 2019 Jacob Jorgensen. All rights reserved.
+//
+
+import UIKit
+
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+}
